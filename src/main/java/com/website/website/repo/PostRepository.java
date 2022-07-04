f4e12e5b28f22c62a78c033bdd2d725c9e4e45b9
IlyaBes <ilyabes@list.ru>
2022-07-04 21:55:45 +0300

    website
    website

---
+++ src/main/java/com/website/website/repo/PostRepository.java
@@ -0,0 +1,10 @@
+package com.website.website.repo;
+
+import com.website.website.models.Post;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PostRepository extends CrudRepository<Post, Long> {
+
+
+
+}
